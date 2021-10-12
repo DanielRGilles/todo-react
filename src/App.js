26f75bf6271ffd62147fb5b8a33941e4b80342f0
DanielRGilles <danryangilles@gmail.com>
2021-10-12 11:18:29 -0700

    added react router dom links

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {
+    BrowserRouter as Router, 
+    Route, 
+    Switch,
+} from 'react-router-dom';
+import Home from './Home.js';
+import Signup from './Signup.js';
+import Login from './Login.js';
+import Todos from './Todos.js';
+import { NavLink } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+              <header>
+              <NavLink
+              exact className='links'
+              activeStyle={{fontSize:'1.5rem'}}
+              to='/'>Home
+              </NavLink>
+              <NavLink
+              exact className='links'
+              activeStyle={{fontSize:'1.5rem'}}
+              to='/Signup'>Signup
+              </NavLink>
+              <NavLink
+              exact className='links'
+              activeStyle={{fontSize:'1.5rem'}}
+              to='/Login'>Login
+              </NavLink>
+              <NavLink
+              exact className='links'
+              activeStyle={{fontSize:'1.5rem'}}
+              to='/Todos'>Todos
+              </NavLink>
+            </header>
+                <Router>
+                    <Switch>
+                        <Route 
+                            path="/" 
+                            exact
+                            render={(routerProps) => <Home {...routerProps} />} 
+                        />
+                        <Route 
+                            path="/Signup" 
+                            exact
+                            render={(routerProps) => <Signup {...routerProps} />} 
+                        />
+                        <Route 
+                            path="/Login" 
+                            exact
+                            render={(routerProps) => <Login {...routerProps} />} 
+                        />
+                        <Route 
+                          path="/Todos/:myId" 
+                          exact
+                          render={(routerProps) => <Todos {...routerProps} />} 
+                        />
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
 }
-
-export default App;
